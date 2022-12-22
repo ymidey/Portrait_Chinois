5b258dad469b0a4426ff0f1fd5c72c87a6341e23
ymidey <midey.yannick@gmail.com>
2022-12-22 22:20:35 +0100

    first commit

---
+++ readme.md
@@ -0,0 +1,45 @@
+# SAE 1.05 : Portrait Chinois 
+
+
+
+Projet réalisé dans le cadre de la SAE 1.05 par Yannick Midey étudiant de 1ère années de BUT MMI.
+
+
+
+# Langage de programmation 
+
+- HTML5, CSS3, JavaScript
+
+
+
+# Objectif du projet
+
+Réalisation d’un site web sur le thème du portrait chinois.
+
+Le portrait chinois consiste à décrire votre caractère et vos goûts au travers d’analogies du type « Si j’étais une couleur, je serais… » « Si j’étais un film, je serais… ». À ces analogies vous ajouterez un passage de type « parce que » explicitant les analogies que vous avez choisies.
+
+# Lien du site web
+
+https://etudiant.u-pem.fr/~yannick.midey/portrait-chinois/
+
+# Installation du projet sur un serveur XAMPP
+
+##### Pour récupérer le projet et l'installer sur un serveur local XAMPP, il faudra réaliser ses actions :
+
+- Installer XAMPP sur votre ordinateur si ce n'est pas déjà fait.
+
+- Installer le projet "portrait_chinois_Yannick_Midey"
+
+- Lancer l'application XAMPP
+
+- Dans la partie droite de XAMPP, cliquer sur l'onglet "explorer", suite à cela, une nouvelle fenêtre s'ouvrira, vous emmenant directement vers les sous dossiers de XAMPP.
+
+- Dans tout ses sous dossiers, chercher le dossier nommé "htdocs" et déposer y votre dossier "portrait_chinois" téléchargé antérieurement
+
+- Ensuite, lancer le module "Apache" en cliquant sur le bouton "start" correspondant à ce module
+
+- Enfin, dans votre navigateur tapez l'adresse suivante : 
+
+  http://localhost/portrait-chinois_Yannick_Midey/
+
+  
---
+++ script/script.js
@@ -0,0 +1,56 @@
+var analogie = "";
+var numCase = 0;
+
+fetch('script/analogies.json').then(function (response) {
+    response.json().then(function (data) {
+        for (numCase; numCase < data.length; numCase++) {
+            const element = data[numCase]
+            document.querySelector(".content").innerHTML += "<section class='section_analogie " + element.id + "'> <h2> Si j'étais " + element.analogie + ", je serais... </h2> <h1>" + element.valeurAnalogie + "</h1> <p class='explication'> " + element.explication + "</p><img src=" + element.image + "></section>"
+        }
+    })
+})
+
+const button = document.querySelector("a.scroll-down-button");
+
+button.addEventListener("click", function () {
+    window.scrollBy({ top: window.innerHeight, behavior: 'smooth' });
+});
+
+var mentionLegales = document.querySelectorAll("p.legals")
+
+for (let i = 0; i < mentionLegales.length; i++) {
+    mentionLegales[i].addEventListener("click", function () {
+        const modal = document.querySelector('.mentions_legales');
+        modal.classList.remove("modal-invisible");
+        modal.classList.add("modal-visible");
+    });
+}
+
+const closeMentionsLegales = document.querySelector(".closeMentionsLegales")
+
+closeMentionsLegales.addEventListener("click", function () {
+    const modal = document.querySelector('.mentions_legales')
+    modal.classList.remove("modal-visible")
+    modal.classList.add("modal-invisible")
+})
+
+/* Valeur de mon formulaire */
+const resAnalogie = document.querySelector("#analogie");
+const resValeurAnalogie = document.querySelector("#valeurAnalogie");
+const resExplication = document.querySelector("#explication");
+const resImage = document.querySelector("#image");
+const resEmail = document.querySelector("#email");
+
+const submit = document.querySelector("#submit")
+
+submit.addEventListener("click", function () {
+    let urlVisitee = "https://perso-etudiant.u-pem.fr/~gambette/portrait/api.php?format=json&courriel=" + resEmail.value + "&login=yannick.midey&message=Si jétais " + resAnalogie.value + ", je serais " + resValeurAnalogie.value + "," + resExplication.value + ". L'urld de l'image que vous nous avez fourni : " + resImage.value;
+    if (window.confirm("Voici les informations que vous nous avez transmis : Si vous êtes " + resAnalogie.value + ", vous serez " + resValeurAnalogie.value + ". Votre explication : " + resExplication.value + ". L'url de votre image : " + resImage.value + ". Votre adresse mail : " + resEmail.value + ". Etes-vous sûr de vouloir envoyer ses informations ?")) {
+        fetch(urlVisitee).then(function (response) {
+            response.json().then(function (data) {
+                console.log("Réponse reçue : ");
+                console.log(data);
+            });
+        });
+    }
+}) 
