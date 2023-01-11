a0765ebf6a1fb65982f3088df0a2309fea407a80
ymidey <77786954+ymidey@users.noreply.github.com>
2023-01-11 14:46:10 +0100

    Update

--- readme.md
+++ readme.md
@@ -20,7 +20,7 @@ Le portrait chinois consiste à décrire votre caractère et vos goûts au trave
 
 # Lien du site web
 
-https://etudiant.u-pem.fr/~yannick.midey/portrait-chinois_Yannick_Midey/
+https://etudiant.u-pem.fr/~yannick.midey/portrait-chinois/
 
 # Installation du projet sur un serveur XAMPP
 
@@ -34,7 +34,7 @@ https://etudiant.u-pem.fr/~yannick.midey/portrait-chinois_Yannick_Midey/
 
 - Dans la partie droite de XAMPP, cliquer sur l'onglet "explorer", suite à cela, une nouvelle fenêtre s'ouvrira, vous emmenant directement vers les sous dossiers de XAMPP.
 
-- Dans tout ses sous dossiers, chercher le dossier nommé "htdocs" et déposer y votre dossier "portrait-chinois_Yannick_Midey" téléchargé antérieurement
+- Dans tout ses sous dossiers, chercher le dossier nommé "htdocs" et déposer y votre dossier "portrait_chinois" téléchargé antérieurement
 
 - Ensuite, lancer le module "Apache" en cliquant sur le bouton "start" correspondant à ce module
 
--- script/script.js
+++ script/script.js
@@ -1,6 +1,7 @@
 var analogie = "";
 var numCase = 0;
 
+/* Récupération des données contenues dans analogies.json*/
 fetch('script/analogies.json').then(function (response) {
     response.json().then(function (data) {
         for (numCase; numCase < data.length; numCase++) {
@@ -10,14 +11,14 @@ fetch('script/analogies.json').then(function (response) {
     })
 })
 
+/* Code permettant suite à un clic sur le bouton avec la class "scroll-down-button" de réaliser un scroll doux automatique vers le bas */
 const button = document.querySelector("a.scroll-down-button");
-
 button.addEventListener("click", function () {
     window.scrollBy({ top: window.innerHeight, behavior: 'smooth' });
 });
 
+/* Code permettant d'ajouter la class "modal-visible" et de retirer la class "modale-invisble" à la div ayant la class "mentions_legales" si un clique est effectué sur l'un des deux paragraphes ayant la class "legals" */
 var mentionLegales = document.querySelectorAll("p.legals")
-
 for (let i = 0; i < mentionLegales.length; i++) {
     mentionLegales[i].addEventListener("click", function () {
         const modal = document.querySelector('.mentions_legales');
@@ -26,15 +27,15 @@ for (let i = 0; i < mentionLegales.length; i++) {
     });
 }
 
+/* Code permettant de retirer la class "modal-visible" et d'ajouter la class "modal-invisible" a la div ayant la class "mentions_legales" afin de rendre la div contenant mon modal invisible, si un clique est effectué sur le bouton ayant la class "closeMentionsLegals" */
 const closeMentionsLegales = document.querySelector(".closeMentionsLegales")
-
 closeMentionsLegales.addEventListener("click", function () {
     const modal = document.querySelector('.mentions_legales')
     modal.classList.remove("modal-visible")
     modal.classList.add("modal-invisible")
 })
 
-/* Valeur de mon formulaire */
+/* Récupération des valeurs de mon formulaire */
 const resAnalogie = document.querySelector("#analogie");
 const resValeurAnalogie = document.querySelector("#valeurAnalogie");
 const resExplication = document.querySelector("#explication");
@@ -43,6 +44,7 @@ const resEmail = document.querySelector("#email");
 
 const submit = document.querySelector("#submit")
 
+/* Code permettant l'envoi à l'api des informations récupérées dans le formulaire ayant la class "form" */
 submit.addEventListener("click", function () {
     let urlVisitee = "https://perso-etudiant.u-pem.fr/~gambette/portrait/api.php?format=json&courriel=" + resEmail.value + "&login=yannick.midey&message=Si jétais " + resAnalogie.value + ", je serais " + resValeurAnalogie.value + "," + resExplication.value + ". L'urld de l'image que vous nous avez fourni : " + resImage.value;
     if (window.confirm("Voici les informations que vous nous avez transmis : Si vous êtes " + resAnalogie.value + ", vous serez " + resValeurAnalogie.value + ". Votre explication : " + resExplication.value + ". L'url de votre image : " + resImage.value + ". Votre adresse mail : " + resEmail.value + ". Etes-vous sûr de vouloir envoyer ses informations ?")) {
@@ -53,5 +55,27 @@ submit.addEventListener("click", function () {
             });
         });
         document.getElementById("myForm").reset();
+        const response = document.querySelector('.response');
+        response.classList.remove("response_invisible");
+        response.classList.add("response_visible");
     }
-}) 
+})
+
+
+const content = document.querySelector('.content');
+const btn_Up = document.getElementById('scroll-up-button');
+
+/* Code permettant d'afficher le bouton ayant la class "scroll-up-button" uniquement si */
+window.addEventListener('scroll', () => {
+    if (window.scrollY >= content.offsetTop) {
+        btn_Up.classList.remove('hidden');
+    } else {
+        btn_Up.classList.add('hidden');
+    }
+});
+
+/* Code permettant à l'aide d'un clique sur le bouton ayant la class "scroll-up-button" de remonter l'utilisateur tout en haut de la page */
+btn_Up.addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
